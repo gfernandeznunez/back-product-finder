c6b4d48b13fb8cae0dfe470f838ea2eea788b3c6
gonzalo <gonzalo.fernandez@tide.cl>
2020-11-25 21:54:57 -0300

    mapeo modelo productos con bd productiva

--- src/main/java/com/walmart/search/model/Product.java
+++ src/main/java/com/walmart/search/model/Product.java
@@ -3,19 +3,25 @@ package com.walmart.search.model;
 import org.springframework.data.annotation.Id;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Builder
 @Data
+@Document(collection="products")
 public class Product {
 
     @Id
-    private int id;
+    private String _id;
 
+    private int id;
     private String brand;
     private String description;
     private String image;
     private Double price;
 
+    //Price with discount
     private Double priceWithDiscount;
+
+    //% of discount
     private Double discount ;
 }
